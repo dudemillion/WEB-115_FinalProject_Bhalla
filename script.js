08bb2e2f15f4010b6adea2317b1d2ed46901bc05
Nikhil Bhalla <nbhalla@my.waketech.edu>
2026-05-06 14:04:48 -0400

    day 7 work

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const gamediv = document.getElementById("game");
 const animframes = ["", "Lpaw", "", "Rpaw"];
 let selected = null;
 let selectedcat = document.createElement("p");
+let money = 0;
 selectedcat.innerHTML = "Selected Cat: ";
 selectedcat.style.fontFamily = "Momentz";
 selectedcat.style.position = "fixed";
@@ -38,6 +39,7 @@ class Cat {
         this.image = newCat;
         newCat.addEventListener("click", (event) => {
             if (this.moving) return;
+            if (this.busy) return;
             event.stopPropagation();
             if (selected !== this) {
                 selectedcat.innerHTML = "Selected Cat: " + this.name;
@@ -65,7 +67,6 @@ class Cat {
     }
     moveTo(target) {
         this.moving = true;
-        let rect = this.image.getBoundingClientRect();
         let startX = parseInt(this.position[0]);
         let startY = parseInt(this.position[1]);
         let dist = Math.hypot(target[0] - startX, target[1] - startY);
@@ -82,15 +83,18 @@ class Cat {
             fill: 'forwards',
             easing: 'linear'
         });
-        movement.onfinish = () => {
+        setTimeout(() => {
             clearInterval(anim);
             this.moving = false;
-            console.log(this.moving);
             this.image.src = "media/" + this.pelt + this.spritedir + ".png";
             this.index = 0;
             this.position = [target[0] + "px", target[1] + "px"]
-        }
+        }, dura);
+        return movement;
     }
+}
+function updateMoney() {
+    document.getElementById("moneycount").innerHTML = "$" + money;
 }
 class Customer {
     constructor(name, personality, position, order, variation, happiness, spritedir, sprite, busy, givenorder, playedcat) {
@@ -109,10 +113,11 @@ class Customer {
     renderCustomer() {
         let newCustomer = document.createElement("img");
         newCustomer.src = this.sprite;
-        newCustomer.style.maxHeight = "200px";
+        newCustomer.style.maxHeight = "150px";
         newCustomer.style.position = "absolute";
         newCustomer.style.left = this.position[0] + "px";
         newCustomer.style.top = this.position[1] + "px";
+        this.image = newCustomer;
         let infobox = document.createElement("div");
         this.infobox = infobox;
         infobox.innerHTML = "Name: " + this.name + "<br>" + "Order: " + this.order + "<br>" + "You have " + (this.givenorder ? "" : "not") + " given them their order." + "<br>" + "Personality: " + this.personality + "<br>" + "Happiness: " + this.happiness;
@@ -141,21 +146,41 @@ class Customer {
             infobox.hidden = true;
             newCustomer.style.borderStyle = "none";
         });
-        newCustomer.addEventListener("click", () => {
+        newCustomer.addEventListener("click", (event) => {
+            event.stopPropagation();
             if (this.busy) return;
             newCustomer.style.borderStyle = "none";
             if (selected) {
-                let thiscat = selected;
-                this.matchPersonality(thiscat);
-                this.busy = true;
-                setTimeout(() => {
-                    thiscat.busy = false;
-                    this.busy = false;
-                }, 10000);
+                if (!this.playedcat) {
+                    let thiscat = selected;
+                    this.matchPersonality(thiscat);
+                    this.busy = true;
+                    thiscat.busy = true;
+                    thiscat.moveTo([this.position[0], this.position[1] + 90]).onfinish = () => {
+                        thiscat.image.style.filter = "grayscale(100%)";
+                        console.log("set gray");
+                    };
+                    selected = null;
+                    thiscat.image.style.borderStyle = "none";
+                    selectedcat.innerHTML = "Selected Cat: ";
+                    setTimeout(() => {
+                        thiscat.busy = false;
+                        this.busy = false;
+                        thiscat.image.style.filter = "grayscale(0%)";
+                        this.updateInfo();
+                        updateMoney();
+                    }, 10000);
+                } else {
+                    console.log("Customer already played with cat!");
+                }
             } else {
                 console.log("No selected cat.")
             }
         });
+        let happinessdown = setInterval(() => {
+            this.happiness -= 1;
+            this.updateInfo();
+        }, 15000);
         gamediv.appendChild(newCustomer);
     }
     updateInfo() {
@@ -168,10 +193,11 @@ class Customer {
             if (cat.personality == this.personality) {
                 console.log("They match! Happiness increases.");
                 this.happiness += 3;
-                this.updateInfo();
+                money += 3;
                 return true;
             } else {
-                console.log("They don't match. Customer happiness remains.");
+                console.log("They don't match. Customer happiness down.");
+                this.happiness -= 2;;
                 return false;
             }
         } else {
@@ -182,13 +208,21 @@ class Customer {
         if (given) {
             if (given == this.order) {
                 console.log("Order matches! Customer happiness increases.");
-                this.happiness += 1;
+                this.happiness += 2;
+                money += 5;
+                updateMoney();
             } else {
-                console.log("They don't match. Customer happiness remains.");
+                console.log("They don't match. Customer happiness down.");
+                this.happiness -= 2;
             }
         } else {
             console.log("Error! Order is undefined!")
         }
+    }
+    leave() {
+        this.image.remove();
+        this.infobox.remove();
+        customers = customers.filter(c => c !== this);
     }
 }
 let cats = [
@@ -236,11 +270,44 @@ start.addEventListener("click", function () {
     cap1.style.display = "none";
     cap2.style.display = "none";
     document.body.style.setProperty("--before-image", "''")
-    gamediv.append(selectedcat);
+    gamediv.appendChild(selectedcat);
+    let moneyicon = document.createElement("img");
+    moneyicon.src = "media/money.png";
+    moneyicon.style.maxHeight = "75px";
+    moneyicon.style.position = "fixed";
+    moneyicon.style.top = "10px";
+    moneyicon.style.right = "10px";
+    let moneycount = document.createElement("p");
+    moneycount.id = "moneycount";
+    moneycount.innerHTML = "$0";
+    moneycount.style.fontFamily = "Baloo";
+    moneycount.style.fontSize = "30px";
+    moneycount.style.color = "rgb(9, 109, 0)";
+    moneycount.style.position = "fixed";
+    moneycount.style.top = "0px";
+    moneycount.style.right = "90px";
+    moneycount.style.textAlign = "right";
+    gamediv.appendChild(moneycount);
+    gamediv.appendChild(moneyicon);
     newcustomer();
     let customerflow = setInterval(() => {
         newcustomer();
-    }, 45000);
+    }, 450000);
+    async function happinesscheck() {
+        while (true) {
+            for (const c of customers) {
+                if (c.happiness == 0) {
+                    console.log("GAME OVER!");
+                    break;
+                } else {
+                    console.log("everyones happy");
+                    continue;
+                }
+            }
+            await new Promise(resolve => setTimeout(resolve, 999999999999));
+        }
+    }
+    happinesscheck();
 })
     
 
